2298cfaa19b87f761236a07c64f1e69759241648
Jamal Johnson <jamal@Edwards-MacBook-Pro.local>
2016-02-03 21:34:54 -0800

    added rf using the forests in mammoth

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ import numpy as np
 import os 
 from sklearn.naive_bayes import GaussianNB
 from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
 import dataIO as data
 
 def gaussianNBayes(x, y, x_predict):
@@ -19,7 +20,10 @@ def ridgeRegression(x, y, x_predict, in_alpha):
     clf = linear_model.Ridge (alpha = in_alpha)
     return (clf.fit(x, y)).predict(x_predict)
 
-
+def randomForest(x, y, x_predict):
+    # There are like 20+ parameters that can be tuned for this model :'(
+    rf = RandomForestClassifier()
+    return rf.fit(x, y).predict(x_predict)
 
 
 def exampleUsage():
@@ -29,3 +33,5 @@ def exampleUsage():
                          training_data['xlabels'])
     out1 = ridgeRegression(training_data['xlabels'], training_data['ylabels'], \
                          training_data['xlabels'], 3)
+    out2 = randomForest(training_data['xlabels'], training_data['ylabels'], \
+                         training_data['xlabels'])
