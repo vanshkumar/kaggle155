5ebd6de79068de0880f8ed1e6dca546361a2dfe1
Vansh Kumar <vanshkumar95@gmail.com>
2016-02-05 14:35:27 -0800

    added function that thresholds regression outputs

---
+++ regress_to_class.py
@@ -0,0 +1,34 @@
+import dataIO as data
+import numpy as np
+
+fname = raw_input('Enter file name: ') 
+print fname
+
+regress_data = open(fname+'.csv', 'r+')
+class_data   = open(fname[:fname.index('_')] + str('_r2c_') + \
+    fname[fname.rindex('_')+1:] + '.csv', 'w+')
+
+regress_data.readline()
+
+y = []
+
+for line in regress_data.readlines():
+    vals = line.split(',')
+    y.append(float(vals[1][:-1]))
+
+y = np.array(np.array(y) >= 0.5, dtype=int)
+
+class_data.write('Id,Prediction\n')
+for i in range(len(y)):
+    class_data.write(str(i+1) + ',' + str(y[i]) + '\n')
+
+regress_data.close()
+class_data.close()
+
+if 'train' in fname:
+    full_train = data.loadTraining()
+    x_train = full_train['xlabels']
+    y_train = full_train['ylabels']
+
+    print "Thresholded training score is: "
+    print np.mean(y_train == y)
