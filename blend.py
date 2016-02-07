e7e50c55ef153a28161ee0f51b407265675f8be3
Anish <Anish@Anishs-MacBook-Pro.local>
2016-02-07 15:51:46 -0800

    more blending

--- blend.py
+++ blend.py
@@ -11,9 +11,25 @@ from sklearn.svm import SVC
 def loadModelOut(filename):	
 	return np.loadtxt(os.getcwd() + '/../WordSentiment/' + filename, delimiter=',', skiprows=1, unpack=True)[1]
 
-# load actual data properly 
-trainingDataFiles = ['ada_regress_train.csv', 'gnb_class_train.csv', 'knn_regress_train.csv', 'log_regress_train.csv']
-testDataFiles = ['ada_regress_test.csv', 'gnb_class_test.csv', 'knn_regress_test.csv', 'log_regress_test.csv']
+trainingDataFiles = ['ada_regress_train.csv', 
+					 'gnb_class_train.csv', 
+					 'knn_regress_train.csv', 
+					 'log_regress_train.csv', 
+					 'nn_class_train.csv',
+					 'rf_regress_train.csv',
+					 'rf_regress_train_2.csv',
+					 'svm_regress_train.csv'
+					 ]
+
+testDataFiles = ['ada_regress_test.csv', 
+				 'gnb_class_test.csv', 
+				 'knn_regress_test.csv', 
+				 'log_regress_test.csv', 
+				 'nn_class_test.csv',
+				 'rf_regress_test.csv',
+				 'rf_regress_test_2.csv',
+				 'svm_regress_test.csv'
+				 ]
 
 y_train = data.loadTraining()['ylabels']
 x_train = np.zeros((len(trainingDataFiles), len(y_train)))
@@ -24,9 +40,11 @@ x_test = np.transpose([loadModelOut(model) for model in testDataFiles])
 
 
 
-parameters = {'C': np.logspace(-4.0, 4.0, 10),
+parameters = {'C': np.logspace(-4.0, 4.0, 20),
+              # 'kernel': ['rbf'],
               'kernel': ['linear', 'poly', 'rbf'],
-              'degree': range(2, 7),
+              # 'degree': [2]
+              'degree': np.arange(0.0, 4.0, 1),
               }
 
 kf_total = cross_validation.KFold(len(x_train), n_folds=5,\
@@ -37,13 +55,19 @@ svm_class = GridSearchCV(estimator=SVC(), \
 
 svm_class.fit(x_train, y_train)
 
+print 'done fit'
+
 cross_val_scores = cross_validation.cross_val_score(estimator=svm_class,\
-    X=x_train, y=y_train, cv=kf_total, n_jobs=1)
+    X=x_train, y=y_train, cv=kf_total, n_jobs=-1)
 
 print "cross val scores: "
 print cross_val_scores
 
 y_test = svm_class.predict(x_test)
+
+g = open('svm_regress_params.txt', 'w+')
+g.write(str(svm_class.best_estimator_.get_params()))
+g.close()
 
 
 f = open('blended_solution.csv', 'w+')
