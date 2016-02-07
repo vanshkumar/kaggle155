f1f33bcc42b4ec5cdf5c3567f47990c82ca4d107
Vansh Kumar <vanshkumar95@gmail.com>
2016-02-07 13:12:33 -0800

    diff grid for svm

--- svm.py
+++ svm.py
@@ -12,16 +12,17 @@ y_train = full_train['ylabels']
 full_test  = data.loadTest()
 x_test = full_test['xlabels']
 
-parameters = {'C': np.logspace(-4.0, 4.0, 20),
-              'kernel': ('linear', 'poly', 'rbf'),
+parameters = {'kernel': ('poly', 'rbf', 'sigmoid'),
               'degree': range(2, 7),
+              'epsilon': np.logspace(-2, 0, 10),
+              'shrinking': (True, False)
               }
 
 kf_total = cross_validation.KFold(len(x_train), n_folds=10,\
       shuffle=True, random_state=4)
 
 svm_class = GridSearchCV(estimator=SVR(), \
-    param_grid=dict(parameters), n_jobs=1, cv=None)
+    param_grid=dict(parameters), n_jobs=-1, cv=None)
 
 svm_class.fit(x_train, y_train)
 
@@ -31,18 +32,18 @@ cross_val_scores = cross_validation.cross_val_score(estimator=svm_class,\
 print "cross val scores: "
 print cross_val_scores
 
-f = open('svm_regress_test.csv', 'w+')
+f = open('svm2_regress_test.csv', 'w+')
 f.write('Id,Prediction\n')
 y_test = svm_class.predict(x_test)
 for i in range(len(y_test)):
     f.write(str(i+1) + ',' + str(y_test[i]) + '\n')
 f.close()
 
-g = open('svm_regress_params.txt', 'w+')
+g = open('svm2_regress_params.txt', 'w+')
 g.write(str(svm_class.best_estimator_.get_params()))
 g.close()
 
-h = open('svm_regress_train.csv', 'w+')
+h = open('svm2_regress_train.csv', 'w+')
 h.write('Id,Prediction\n')
 y_test_est = svm_class.predict(x_train)
 for i in range(len(y_test_est)):
